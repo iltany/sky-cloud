4568aff6337141c29978424f5dd9ba00d33ee0da
Olya Kalun <olyakalun1998@gmail.com>
2021-12-14 13:09:50 +0200

    Styled FAQ page

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
-import { Accordion, Icon } from "semantic-ui-react";
+import { Accordion } from "semantic-ui-react";
 import StatsPanel from "../../components/stats-panel/StatsPanel";
 import { FAQ_MOCK } from "./MOCK";
 import useStyles from "./styles";
+import Plus from "../../assets/icons/Plus";
+import Minus from "../../assets/icons/Minus";
 
 const Faq = () => {
   const classes = useStyles();
-  const [active, setActive] = useState(false);
-  const handleClick = () => {
-    setActive(!active);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    setActiveIndex(newIndex);
   };
 
   return (
@@ -18,15 +24,25 @@ const Faq = () => {
         <div className="page-subtitle">Online storage</div>
         <div className={classes.accordionContainer}>
           <Accordion styled>
-            {FAQ_MOCK.map((item) => {
+            {FAQ_MOCK.map((item, index) => {
               return (
                 <>
-                  <Accordion.Title key={item.id} onClick={handleClick}>
-                    <Icon name="dropdown" />
+                  <Accordion.Title
+                    key={item.id}
+                    onClick={handleClick}
+                    index={index}
+                    active={activeIndex === index}
+                    className={classes.accordionTitle}
+                  >
+                    {activeIndex === index ? <Minus /> : <Plus />}
+
                     {item.title}
                   </Accordion.Title>
-                  <Accordion.Content>
-                    <p>{item.description}</p>
+                  <Accordion.Content
+                    active={activeIndex === index}
+                    className={classes.accordionContent}
+                  >
+                    {item.description}
                   </Accordion.Content>
                 </>
               );
--- src/pages/faq/styles.js
+++ src/pages/faq/styles.js
@@ -27,12 +27,81 @@ const useStyles = createUseStyles({
   stats: {
     marginTop: 247,
     "@media (max-width: 992px)": {
-      marginTop: 157,
+      marginTop: 209,
     },
   },
   accordionContainer: {
     display: "flex",
     justifyContent: "center",
+    "& .accordion.ui.styled": {
+      maxWidth: 1199,
+      width: "100%",
+      borderRadius: 15,
+      boxShadow: "none",
+      margin: "0px 13px 0px 18px",
+    },
+    "& .ui.styled.accordion > div:nth-child(4n+1)": {
+      background: "linear-gradient(90deg, #e8f7ff, #f5ecff)",
+      opacity: 1,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15,
+    },
+    "& .ui.styled.accordion > div": {
+      background: "linear-gradient(90deg, #f0faff, #f9f2ff)",
+      opacity: 1,
+    },
+    "& .ui.styled.accordion > div:nth-last-child(-n+2)": {
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15,
+    },
+  },
+  accordionTitle: {
+    ".ui.styled.accordion &.title": {
+      display: "flex",
+      flexDirection: "row-reverse",
+      alignItems: "center",
+      justifyContent: "space-between",
+      fontFamily: "Comfortaa",
+      fontSize: 30,
+      fontWeight: 700,
+      lineHeight: "33px",
+      color: "#46637E",
+      padding: "28px 43px 26px 54px",
+      marginBottom: 13,
+      border: "none",
+      "&:first-child": {
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        borderBottomLeftRadius: "unset",
+        borderBottomRightRadius: "unset",
+      },
+    },
+    "& svg": {
+      width: 31,
+      height: 31,
+      "@media (max-width: 992px)": {
+        width: 13,
+      },
+    },
+    ".ui.styled.accordion &.active.title": {
+      background: "linear-gradient(90deg, #B1E3FF 0%, #DFBEFF 100%)",
+      opacity: 1,
+    },
+  },
+  accordionContent: {
+    ".ui.styled.accordion &.content": {
+      fontFamily: "Comfortaa",
+      fontSize: 20,
+      fontWeight: 400,
+      lineHeight: "30px",
+      color: "#878787",
+      padding: "31px 54px 90px 54px",
+      background: "none",
+      opacity: 1,
+      "@media (max-width: 992px)": {
+        padding: "18px 25px 31px 22px",
+      },
+    },
   },
 });
 
